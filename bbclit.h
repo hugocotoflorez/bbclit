6fc8d6042e12477ee065537885ee66466835a503
hugocotoflorez <hugocoto100305@gmail.com>
2024-03-23 15:02:23 +0100

    update

--- bbclit.h
+++ bbclit.h
@@ -2,17 +2,65 @@
 #define BCLI_H
 
 
-typedef struct __DIMENSIONS
+// %2d;%2d;%2d where each %d can be color or style format
+#define COLOR_FORMAT(a) char a[8]
+
+/**
+ * Dimensions
+ * X is horizontal axis
+ * Y is vertical axis
+ */
+typedef struct __DIMENSION
 {
     int x0, x1, y0, y1;
-} DIMENSIONS;
+} DIMENSION;
 
 
 typedef struct __BOX
 {
-    DIMENSIONS size;
+    DIMENSION size;
+    struct __BOX* parent;
     struct __BOX* attached_boxes;
 } BOX;
+
+
+// CUSTOMIZE SETTINGS
+
+struct CORNER
+{
+    char top_left, top_right, bottom_left, bottom_right;
+};
+
+struct BORDER
+{
+    char vertical, horizontal;
+};
+
+struct INTERSECTION
+{
+    char vertical_up, vertical_down, horizontal_right, horizontal_left, full_intersection;
+};
+
+struct COLOR
+{
+    COLOR_FORMAT(border);
+    COLOR_FORMAT(text);
+    COLOR_FORMAT(text2);
+    COLOR_FORMAT(title);
+};
+
+
+typedef struct __CUSTOMIZE_SETTINGS
+{
+    struct CORNER corner;
+    struct BORDER border;
+    struct INTERSECTION intersection;
+    struct COLOR color;
+
+} CUSTOMIZE_SETTINGS;
+
+
+extern CUSTOMIZE_SETTINGS DEFAULT_CUSTOMIZE_SETTINGS();
 
 
 #endif // !BCLI_H
