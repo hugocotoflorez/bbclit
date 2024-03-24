c38c95a194321fd438f56f12ac365100b0c32387
hugocotoflorez <hugocoto100305@gmail.com>
2024-03-24 18:55:06 +0100

    AutoUpdate

--- drawing.c
+++ drawing.c
@@ -51,7 +51,39 @@ DIMENSION relative_to_absolute_size(BOX* self_box)
 }
 
 
-short get_intersection(short c1, short c2)
+wchar_t get_character_by_id(struct CUSTOMIZE_SETTINGS settings, short id)
+{
+    switch(id)
+    {
+        case 3:
+            return settings.corner.top_left;
+        case 5:
+            return settings.border.vertical;
+        case 6:
+            return settings.corner.bottom_left;
+        case 7:
+            return settings.intersection.vertical_left;
+        case 9:
+            return settings.corner.top_right;
+        case 10:
+            return settings.border.horizontal;
+        case 11:
+            return settings.intersection.horizontal_down;
+        case 12:
+            return settings.corner.bottom_right;
+        case 13:
+            return settings.intersection.vertical_right;
+        case 14:
+            return settings.intersection.horizontal_up;
+        case 15:
+            return settings.intersection.full_intersection;
+        default:
+            return L'\0';
+    }
+}
+
+
+short get_intersection_id(short wcID1, short wcID2)
 {
     /*
      * Intersections as
@@ -66,54 +98,27 @@ short get_intersection(short c1, short c2)
      * To add intersections just do an or of their binary number
      *
      */
-    return c1 | c2;
+    return wcID1 | wcID2;
 }
 
 
-wchar_t get_character_by_id(struct CUSTOMIZE_SETTINGS settings, short id)
+short get_intersection_char(struct CUSTOMIZE_SETTINGS settings, short wcID1, short wcID2)
 {
-    switch(id)
-    {
-        case 3:
-            return settings.corner.top_left;
-            break;
-        case 5:
-            return settings.border.vertical;
-            break;
-        case 6:
-            return settings.corner.bottom_left;
-            break;
-        case 7:
-            return settings.intersection.vertical_right;
-            break;
-        case 9:
-            return settings.corner.top_right;
-            break;
-        case 10:
-            return settings.border.horizontal;
-            break;
-        case 11:
-            return settings.intersection.horizontal_down;
-            break;
-        case 12:
-            return settings.corner.bottom_right;
-            break;
-        case 13:
-            return settings.intersection.vertical_left;
-            break;
-        case 14:
-            return settings.intersection.horizontal_up;
-            break;
-        case 15:
-            return settings.intersection.full_intersection;
-            break;
-        default:
-            wprintf(L"Cannot find %d character!!!!!", id);
-            exit(1);
-            return L'\0';
-    }
+    /*
+     * Intersections as
+     *        2
+     *   1    0    3
+     *        4
+     *  where numbers represent where characters are attached
+     *  0 is default node
+     *  Example: | is 2,4 -> 0101 -> 5
+     *  Example: - is 1,3 -> 1010 -> 10
+     *
+     * To add intersections just do an or of their binary number
+     *
+     */
+    return get_character_by_id(settings, get_intersection_id(wcID1, wcID2));
 }
-
 
 short get_character_id(struct CUSTOMIZE_SETTINGS settings, wchar_t c)
 {
@@ -153,79 +158,137 @@ void draw_raw_box(DIMENSION global_size, struct CUSTOMIZE_SETTINGS settings, str
     int p;
     cursor_goto(global_size.x0, global_size.y0);
     putwchar(settings.corner.top_left);
-    {
-        screen
-        ->screen_arr[(global_size.x0 - 1) * (screen->size.y1) + global_size.y0 - 1] =
-        get_character_id(settings, settings.corner.top_left);
-    }
+    screen->screen_arr[(global_size.x0 - 1) * (screen->size.y1) + global_size.y0 - 1] =
+    get_character_id(settings, settings.corner.top_left);
     for(p = global_size.x0 + 1; p < global_size.x1; p++)
     {
         putwchar(settings.border.horizontal);
-        {
-            screen->screen_arr[(p - 1) * (screen->size.y1) + global_size.y0 - 1] =
-            get_character_id(settings, settings.border.horizontal);
-        }
+        screen->screen_arr[(p - 1) * (screen->size.y1) + global_size.y0 - 1] =
+        get_character_id(settings, settings.border.horizontal);
     }
     putwchar(settings.corner.top_right);
-    {
-        screen
-        ->screen_arr[(global_size.x1 - 1) * (screen->size.y1) + global_size.y0 - 1] =
-        get_character_id(settings, settings.corner.top_right);
-    }
+    screen->screen_arr[(global_size.x1 - 1) * (screen->size.y1) + global_size.y0 - 1] =
+    get_character_id(settings, settings.corner.top_right);
     for(p = global_size.y0 + 1; p < global_size.y1; p++)
     {
         cursor_goto(global_size.x0, p);
         putwchar(settings.border.vertical);
-        {
-            screen->screen_arr[(global_size.x0 - 1) * (screen->size.y1) + p - 1] =
-            get_character_id(settings, settings.border.vertical);
-        }
+        screen->screen_arr[(global_size.x0 - 1) * (screen->size.y1) + p - 1] =
+        get_character_id(settings, settings.border.vertical);
         cursor_goto(global_size.x1, p);
         putwchar(settings.border.vertical);
-        {
-            screen->screen_arr[(global_size.x1 - 1) * (screen->size.y1) + p - 1] =
-            get_character_id(settings, settings.border.vertical);
-        }
+        screen->screen_arr[(global_size.x1 - 1) * (screen->size.y1) + p - 1] =
+        get_character_id(settings, settings.border.vertical);
     }
     cursor_goto(global_size.x0, global_size.y1);
     putwchar(settings.corner.bottom_left);
-    {
-        screen
-        ->screen_arr[(global_size.x0 - 1) * (screen->size.y1) + global_size.y1 - 1] =
-        get_character_id(settings, settings.corner.bottom_left);
-    }
+    screen->screen_arr[(global_size.x0 - 1) * (screen->size.y1) + global_size.y1 - 1] =
+    get_character_id(settings, settings.corner.bottom_left);
     for(p = global_size.x0 + 1; p < global_size.x1; p++)
     {
         putwchar(settings.border.horizontal);
-        {
-            screen->screen_arr[(p - 1) * (screen->size.y1) + global_size.y1 - 1] =
-            get_character_id(settings, settings.border.horizontal);
-        }
+        screen->screen_arr[(p - 1) * (screen->size.y1) + global_size.y1 - 1] =
+        get_character_id(settings, settings.border.horizontal);
     }
     putwchar(settings.corner.bottom_right);
-    {
-        screen
-        ->screen_arr[(global_size.x1 - 1) * (screen->size.y1) + global_size.y1 - 1] =
-        get_character_id(settings, settings.corner.bottom_right);
-    }
+    screen->screen_arr[(global_size.x1 - 1) * (screen->size.y1) + global_size.y1 - 1] =
+    get_character_id(settings, settings.corner.bottom_right);
     fflush(OUT_STREAM);
-    //......
-    //__DEBUG_print_charId_matix(screen, global_size);
-    //......
 }
 
 
-void draw_child_box()
+void draw_child_box(DIMENSION global_size, struct CUSTOMIZE_SETTINGS settings, struct SCREEN* screen)
 {
+    int p;
+    wchar_t c;
+    cursor_goto(global_size.x0, global_size.y0);
+    putwchar(
+    c = get_intersection_char(settings,
+    get_character_id(settings, settings.corner.top_left),
+    screen->screen_arr[(global_size.x0 - 1) * (screen->size.y1) + global_size.y0 - 1]));
+    {
+        screen
+        ->screen_arr[(global_size.x0 - 1) * (screen->size.y1) + global_size.y0 - 1] =
+        get_character_id(settings, c);
+    }
+    for(p = global_size.x0 + 1; p < global_size.x1; p++)
+    {
+        putwchar(c = get_intersection_char(settings,
+                 get_character_id(settings, settings.border.horizontal),
+                 screen->screen_arr[(p - 1) * (screen->size.y1) + global_size.y0 - 1]));
+        {
+            screen->screen_arr[(p - 1) * (screen->size.y1) + global_size.y0 - 1] =
+            get_character_id(settings, c);
+        }
+    }
+    putwchar(
+    c = get_intersection_char(settings,
+    get_character_id(settings, settings.corner.top_right),
+    screen->screen_arr[(global_size.x1 - 1) * (screen->size.y1) + global_size.y0 - 1]));
+    {
+        screen
+        ->screen_arr[(global_size.x1 - 1) * (screen->size.y1) + global_size.y0 - 1] =
+        get_character_id(settings, c);
+    }
+    for(p = global_size.y0 + 1; p < global_size.y1; p++)
+    {
+        cursor_goto(global_size.x0, p);
+        putwchar(c = get_intersection_char(settings,
+                 get_character_id(settings, settings.border.vertical),
+                 screen->screen_arr[(global_size.x0 - 1) * (screen->size.y1) + p - 1]));
+        {
+            screen->screen_arr[(global_size.x0 - 1) * (screen->size.y1) + p - 1] =
+            get_character_id(settings, c);
+        }
+        cursor_goto(global_size.x1, p);
+        putwchar(c = get_intersection_char(settings,
+                 get_character_id(settings, settings.border.vertical),
+                 screen->screen_arr[(global_size.x1 - 1) * (screen->size.y1) + p - 1]));
+        {
+            screen->screen_arr[(global_size.x1 - 1) * (screen->size.y1) + p - 1] =
+            get_character_id(settings, c);
+        }
+    }
+    cursor_goto(global_size.x0, global_size.y1);
+    putwchar(
+    c = get_intersection_char(settings,
+    get_character_id(settings, settings.corner.bottom_left),
+    screen->screen_arr[(global_size.x0 - 1) * (screen->size.y1) + global_size.y1 - 1]));
+    {
+        screen
+        ->screen_arr[(global_size.x0 - 1) * (screen->size.y1) + global_size.y1 - 1] =
+        get_character_id(settings, c);
+    }
+    for(p = global_size.x0 + 1; p < global_size.x1; p++)
+    {
+        putwchar(c = get_intersection_char(settings,
+                 get_character_id(settings, settings.border.horizontal),
+                 screen->screen_arr[(p - 1) * (screen->size.y1) + global_size.y1 - 1]));
+        {
+            screen->screen_arr[(p - 1) * (screen->size.y1) + global_size.y1 - 1] =
+            get_character_id(settings, c);
+        }
+    }
+    putwchar(
+    c = get_intersection_char(settings,
+    get_character_id(settings, settings.corner.bottom_right),
+    screen->screen_arr[(global_size.x1 - 1) * (screen->size.y1) + global_size.y1 - 1]));
+    {
+        screen
+        ->screen_arr[(global_size.x1 - 1) * (screen->size.y1) + global_size.y1 - 1] =
+        get_character_id(settings, c);
+    }
+    fflush(OUT_STREAM);
 }
 
 
 void draw_box(BOX* self_box, struct SCREEN* screen)
 {
-    DIMENSION global_size = relative_to_absolute_size(self_box);
+    DIMENSION global_size = (self_box->size);
     // check if dimension is possible
     if(self_box->parent == NULL)
-        draw_raw_box(global_size, self_box->settings, screen);
+        draw_child_box(global_size, self_box->settings, screen);
+    // draw_raw_box(global_size, self_box->settings, screen);
     else
-        draw_child_box();
+        draw_child_box(global_size, self_box->settings, screen);
 }
--- main.c
+++ main.c
@@ -16,8 +16,18 @@ int main(int argc, char** arcv)
     clear_screen();
     struct SCREEN screen = initialize_screen();
     DIMENSION size       = fullscreen();
+    DIMENSION size2      = { size.x0, size.x1 / 2, size.y0, size.y1 };
+    DIMENSION size3      = { size2.x0, size2.x1 / 3, size2.y0, size2.y1 };
+    DIMENSION size4      = { size2.x0+10, size2.x1-10, size2.y0 + 10, size2.y1 };
     BOX main_box         = new_box(NULL, size);
+    BOX split            = new_box(NULL, size2);
+    BOX split2           = new_box(NULL, size3);
+    BOX split3           = new_box(NULL, size4);
+
     draw_box(&main_box, &screen);
+    draw_box(&split, &screen);
+    draw_box(&split2, &screen);
+    draw_box(&split3, &screen);
     bool i = false;
     keyboard_handler(i);
     free(screen.screen_arr);
